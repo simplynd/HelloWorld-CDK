a838b05290572faeee02d552923e4a803079bfd5
Nakul Detroja <nakulkumar.detroja@ibm.com>
2024-01-31 14:20:33 -0500

    initial lambda cdk stack

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import aws_cdk as cdk
+
+from hello_world_cdk_app.hello_world_cdk_app_stack import HelloWorldCdkAppStack
+
+
+app = cdk.App()
+HelloWorldCdkAppStack(app, "HelloWorldCdkAppStack")
+
+app.synth()
---
+++ hello_world_cdk_app/hello_world_cdk_app_stack.py
@@ -0,0 +1,19 @@
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+    aws_lambda as _lambda
+)
+
+
+class HelloWorldCdkAppStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # Defines an AWS Lambda resource
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.from_asset('lambda'), # Directory where code is taken from
+            handler='hello.handler', # Filename.method (hello.py for lambda code and handler is the method that will execute first as entry point)
+        )
