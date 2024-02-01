eb1daa84727403ecb81f84fa2b774ae1fc732829
Nakul Detroja <nakulkumar.detroja@ibm.com>
2024-02-01 11:29:39 -0500

    update lambda function

--- lambda/hello.py
+++ lambda/hello.py
@@ -2,10 +2,11 @@ import json
 
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
+    print('context: {}'.format(json.dumps(context)))
     return {
         'statusCode': 200,
         'headers': {
             'Content-Type': 'text/plain'
         },
-        'body': 'Hello, CDK! You have hit {}\n'.format(event['path'])
+        'body': 'Hello From Lambda'
     }
