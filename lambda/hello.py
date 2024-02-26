b1050f72bc0e50cba4951aed64dd143be15bda07
Nakul Detroja <nakulkumar.detroja@ibm.com>
2024-02-26 08:50:55 -0500

    test out actions

--- lambda/hello.py
+++ lambda/hello.py
@@ -3,6 +3,8 @@ import json
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
     print('context: {}'.format(context))
+
+    ## Simple Hello World
     return {
         'statusCode': 200,
         'headers': {
